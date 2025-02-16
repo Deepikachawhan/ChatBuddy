1d7c9374befed66bbfb1459fb30f041f00216fb7
Deepikachawhan <161972915+Deepikachawhan@users.noreply.github.com>
2025-02-16 16:48:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -163,7 +163,26 @@ fileCancelButton.addEventListener("click", () => {
     userData.file = {};
     fileUploadWrapper.classList.remove("file-uploaded");
 });
+// Initialize emoji picker and handle emoji selection
+const picker = new EmojiMart.Picker({
+    theme: "light",
+    skinTonePosition: "none",
+    previewPosition: "none",
+    onEmojiSelect: (emoji) => {
+        const{selectionStart: start, selectionEnd: end} = messageInput;
+        messageInput.setRangeText(emoji.native,start, end, "end");
+        messageInput.focus();
+    },
+    onClickOutside: (e) => {
+        if(e.target.id === "emoji-picker"){
+            document.body.classList.toggle("show-emoji-picker");
+        }else{
+            document.body.classList.remove("show-emoji-picker");
+        }
+    }
+});
 
+document.querySelector(".chat-form").appendChild(picker);
 // Add click event for send button
 sendMessage.addEventListener("click", handleOutgoingMessage);
 document.querySelector("#file-upload").addEventListener("click", () => fileInput.click());
